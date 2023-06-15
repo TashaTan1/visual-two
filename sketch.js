40585507dd91bbecd7fcad30a2bc51cd1a0fcbdf
TashaTan1 <136006359+TashaTan1@users.noreply.github.com>
2023-06-15 10:14:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -42,13 +42,13 @@ function draw() {
   //  console.log(timer);
   if (timer == 0) {
     button = createButton("Back to Frequencies");
-    button.position((width/2) -200, height/2);
+    button.position(1, 1);
     button.mousePressed(function goToAnotherPage() {
       window.location.href =
         "https://tashatan1.github.io/front-page-/";
     });
     button = createButton("Let's Breath");
-    button.position((width/2) +80, height/2);
+    button.position(1350, 1);
     button.mousePressed(function goToAnotherPage() {
       window.location.href =
         "https://tashatan1.github.io/let-s-breath/";
